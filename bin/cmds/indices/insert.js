379c181bac78991ab52163454c7f75018e9060b7
Yannick Schurter <yannick.schurter@gmail.com>
2017-03-16 15:48:43 +0100

    print more metrics on insert

--- bin/cmds/indices/insert.js
+++ bin/cmds/indices/insert.js
@@ -18,19 +18,76 @@ exports.handler = function (argv) {
   if (argv.token) { globalOptions.token = argv.token; }
   if (argv.insecure) { globalOptions.strictSSL = false; }
 
+  const aggs = {
+    total: 0,
+    inserted: 0,
+    updated: 0,
+    failed: 0
+  };
+
   co(function* () {
     for (const file of files) {
+      let res;
       try {
-        yield insertFile(file, indice, globalOptions);
+        res = yield insertFile(file, indice, globalOptions);
       } catch (e) {
         console.error(e);
+        continue;
+      }
+
+      ['total','inserted','updated','failed'].forEach(cat => {
+        res[cat] = parseInt(res[cat]);
+        aggs[cat] += res[cat] || 0;
+      });
+
+      printMetric('Total sent', res.total);
+      printMetric('  Inserted', res.inserted, res.total);
+      printMetric('  Updated', res.updated, res.total);
+      printMetric('  Failed', res.failed, res.total);
+
+      if (res.errors && res.errors.length) {
+        printErrors(res.errors);
       }
     }
+
+    console.log();
+    console.log('Global metrics');
+    console.log('--------------');
+    printMetric('Files', files.length);
+    printMetric('Total sent', aggs.total);
+    printMetric('  Inserted', aggs.inserted, aggs.total);
+    printMetric('  Updated', aggs.updated, aggs.total);
+    printMetric('  Failed', aggs.failed, aggs.total);
+
   }).catch(e => {
     console.error(e);
     process.exit(1);
   });
+
 };
+
+function printMetric(label, value, total) {
+  let msg = `  ${label}: ${value}`;
+
+  if (total) {
+    const percent = Math.round(value / total * 100);
+    msg += ` (${percent}%)`;
+  }
+
+  console.log(msg);
+}
+
+function printErrors(errors) {
+  console.log('  Errors:');
+
+  errors.forEach(error => {
+    let msg = `    ${error.reason || error.type}`;
+    if (error.caused_by) {
+      msg += ` (caused by: ${error.caused_by.reason || error.caused_by.type})`;
+    }
+    console.log(msg);
+  });
+}
 
 function insertFile(file, indice, globalOptions) {
   return co(function* () {
@@ -62,26 +119,8 @@ function insertFile(file, indice, globalOptions) {
       bar.tick(chunk.length, barTokens);
     });
 
-    yield ezmesure.indices.insert(fileReader, indice, options).then(res => {
-      if (!res) {
-        return Promise.reject(new Error('No result'));
-      }
-
-      console.log('  [%s] Inserted', res.inserted);
-      console.log('  [%s] Updated', res.updated);
-      console.log('  [%s] Failed', res.failed);
-
-      if (res.errors && res.errors.length) {
-        console.log('  Errors:');
-
-        res.errors.forEach(error => {
-          let msg = `    ${error.reason || error.type}`;
-          if (error.caused_by) {
-            msg += ` (caused by: ${error.caused_by.reason || error.caused_by.type})`;
-          }
-          console.log(msg);
-        });
-      }
+    return ezmesure.indices.insert(fileReader, indice, options).then(res => {
+      return res || Promise.reject(new Error('No result'));
     });
   });
 }

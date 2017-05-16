82090ac01811126ab19da5811af34365b1a8bcd9
Thomas Porquet <thomas.porquet@couperin.org>
2017-05-16 13:18:00 +0200

    document <command> in the general help

--- bin/cmds/indices.js
+++ bin/cmds/indices.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.command = 'indices <command>';
-exports.desc    = 'Manage indices';
+exports.desc    = 'Manage indices with a <command>: list, insert or delete';
 exports.handler = function (argv) {};
 exports.builder = function (yargs) {
   return yargs.commandDir('indices');

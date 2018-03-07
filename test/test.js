ed261c1b1738c83591a779957af7b9836962ae13
Yannick Schurter <yannick.schurter@gmail.com>
2018-03-07 11:14:09 +0100

    update tests

--- test/test.js
+++ test/test.js
@@ -5,8 +5,9 @@ const path       = require('path');
 const expect     = require('chai').expect;
 const ezmesure   = require('../index.js');
 
-const testFile   = path.join(__dirname, '/test-sample.csv');
-const testFileGZ = path.join(__dirname, '/test-sample-compressed.csv.gz');
+const invalidTestFile = path.join(__dirname, '/test-sample-invalid.csv');
+const testFile        = path.join(__dirname, '/test-sample.csv');
+const testFileGZ      = path.join(__dirname, '/test-sample-compressed.csv.gz');
 
 describe('ezMESURE', () => {
   before(() => {
@@ -25,13 +26,21 @@ describe('ezMESURE', () => {
     });
   });
 
-  it('should correctly create index univ-test from gz file(@03)', () => {
+  it('should fire an error if the input file is invalid (@03)', () => {
+    return ezmesure.indices.insert(invalidTestFile, 'univ-test').then(rep => {
+      return Promise.reject('the request should fail');
+    }).catch(e => {
+      expect(e).to.have.property('statusCode', 400);
+    });
+  });
+
+  it('should correctly create index univ-test from gz file(@04)', () => {
     return ezmesure.indices.insert(testFileGZ, 'univ-test').then(rep => {
       expect(rep).to.have.property('inserted', 5);
     });
   });
 
-  it('should correctly delete index univ-test (@04)', () => {
+  it('should correctly delete index univ-test (@05)', () => {
     return ezmesure.indices.delete('univ-test', rep => {
       expect(rep).to.have.property('acknowledged', true);
     });
